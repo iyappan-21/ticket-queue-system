3e1e84ac7738f82d3d36e770ca85852afd94f83f
iyappan-21 <iyappanaiyappan007@gmail.com>
2026-01-09 16:11:04 +0530

    fix:adding more fun

--- ticket_queue.py
+++ ticket_queue.py
@@ -5,28 +5,91 @@ class Ticket:
         self.issue=issue
         self.priority=priority
 
-
-class Queue:
+class TicketQueue:
     def __init__(self):
-        self.front=None
-        self.rear=None
+       self.critical=[]
+       self.high=[]
+       self.normal=[]
 
 
-    def add(self,ticket):
-        if self.rear is None:
-            self.front=self.rear=ticket
+    def add_ticket(self,ticket):
+        if ticket.priority == "CRITICAL":
+            self.critical.append(ticket)
+        elif ticket.priority == "HIGH":
+            self.high.append(ticket)
+        else:
+            self.normal.append(ticket)
+
+        print("Ticket added:", ticket.ticket_id)
+        
+
+    def resolve_ticket(self):
+        if self.critical:
+            t = self.critical.pop(0)
+        elif self.high:
+            t = self.high.pop(0)
+        elif self.normal:
+            t = self.normal.pop(0)
+        else:
+            print("No tickets to resolve")
             return
-        self.rear.next=ticket
-        self.rear=ticket
 
-    def remove(self):
-        if self.front is None:
+        print("Resolved ticket:", t.ticket_id)
+
+
+    def cancel_ticket(self,ticket_id):
+        for q in[self.critical,self.high,self.normal]:
+            for i in range(len(q)):
+                if q[i].ticket_id==ticket_id:
+                    q.pop(i)
+                    print('cancelled ticket',ticket_id)
+                    return
+        print("Ticket not found")
+
+
+    def find_ticket(self, ticket_id):
+        pos = 1
+        for q in [self.critical, self.high, self.normal]:
+            for t in q:
+                if t.ticket_id == ticket_id:
+                    print("Ticket position:", pos)
+                    return
+                pos += 1
+        print("Ticket not found")
+
+
+    def list_tickets(self):
+        if not (self.critical or self.high or self.normal):
+            print("Queue empty")
             return
-        temp=self.front
-        self.front=self.front.next
-        if self.front is None:
-            self.rear=None
-        temp.next=None
-        return temp
+
+        for t in self.critical:
+            print( t.customer_name)
+        for t in self.high:
+            print(t.customer_name)
+        for t in self.normal:
+            print( t.customer_name)
+
+
+system = TicketQueue()
+
+system.add_ticket(Ticket(1,"C1","Issue","CRITICAL"))
+system.add_ticket(Ticket(2,"C2","Issue","CRITICAL"))
+system.add_ticket(Ticket(3,"H1","Issue","HIGH"))
+system.add_ticket(Ticket(4,"H2","Issue","HIGH"))
+system.add_ticket(Ticket(5,"N1","Issue","NORMAL"))
+system.add_ticket(Ticket(6,"N2","Issue","NORMAL"))
+
+system.list_tickets()
+
+print("\nAdding C3\n")
+system.add_ticket(Ticket(7,"C3","Issue","CRITICAL"))
+
+system.list_tickets()
+
+    
+
+
+
 
         
